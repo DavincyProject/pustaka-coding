0ca453bcba3a60674f5eea6ce8f6f473336e7fa1
Davincy Project <bdavincy.bd@gmail.com>
2023-03-30 16:48:31 +0700

    menambah controller latihan1.php dan viewnya

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -1,14 +1,10 @@
 <?php
-
-namespace AppControllers;
-
-use CI_Controller;
-
 class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        // $this->load->view('view-latihan1');
+        echo "Selamat Datang.. selamat belajar Web Programming";
+        //$this->load->view('view-latihan1');
     }
     public function penjumlahan($n1, $n2)
     {
@@ -16,6 +12,6 @@ class Latihan1 extends CI_Controller
         $data['nilai1'] = $n1;
         $data['nilai2'] = $n2;
         $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
-        $this->load->view('view-latihan1', $data);
+        $this->load->view('view-latihan', $data);
     }
 }
--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -9,8 +9,7 @@
     Halo Kawan.. Yuk kita belajar web programming..!!!<br>
     Nilai 1 = <?= $nilai1; ?>
     Nilai 2 = <?= $nilai2; ?>
-    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
-                                                                $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
 
 </html>

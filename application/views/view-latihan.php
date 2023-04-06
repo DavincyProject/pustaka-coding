eb9a462345f3fc1a2488ad206fa601b7056453db
Davincy Project <bdavincy.bd@gmail.com>
2023-04-06 15:42:06 +0700

    add new file

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -1,14 +1,41 @@
 <html>
 
 <head>
+    <style>
+        html {
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+
+        .container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        p {
+            text-align: center;
+        }
+    </style>
+
     <title>Latihan 1</title>
 </head>
 
 <body>
-    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
-    Nilai 1 = <?= $nilai1; ?> <br>
-    Nilai 2 = <?= $nilai2; ?> <br>
-    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    <div class="container">
+        <h1>Halo Kawan..<br>Yuk kita belajar web programming..!!!</h1>
+
+        <div class="isi">
+            <p>Nilai 1 = <?= $nilai1; ?> <br>
+                Nilai 2 = <?= $nilai2; ?> <br></p>
+            <h4>ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?></h4>
+        </div>
+    </div>
 </body>
 
 </html>

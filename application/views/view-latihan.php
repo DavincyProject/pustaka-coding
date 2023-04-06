4979c963c2c772a1db5337216cb3158f058f82e5
Davincy Project <bdavincy.bd@gmail.com>
2023-04-06 15:30:07 +0700

    add new file

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -2,13 +2,12 @@
 
 <head>
     <title>Latihan 1</title>
-    34
 </head>
 
 <body>
     Halo Kawan.. Yuk kita belajar web programming..!!!<br>
-    Nilai 1 = <?= $nilai1; ?>
-    Nilai 2 = <?= $nilai2; ?>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
     ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
 

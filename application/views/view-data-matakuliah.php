78db04f02ab331a4c3ff6dac1f3670ce92dfdde3
Davincy Project <bdavincy.bd@gmail.com>
2023-04-13 15:14:53 +0700

    add database function crud

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -9,46 +9,46 @@
 </head>
 
 <body>
-
-    <table>
-        <tr>
-            <th colspan="3">
-                Tampil Data Mata Kuliah
-            </th>
-        </tr>
-        <tr>
-            <td colspan="3">
-                <hr>
-            </td>
-        </tr>
-        <tr>
-            <th>Kode MTK</th>
-            <th>:</th>
-            <td>
-                <?= $kode; ?>
-            </td>
-        </tr>
-        <tr>
-            <td>Nama MTK</td>
-            <td>:</td>
-            <td>
-                <?= $nama; ?>
-            </td>
-        </tr>
-        <tr>
-            <td>SKS</td>
-            <td>:</td>
-            <td>
-                <?= $sks; ?>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="3" align="center">
-                <a href="<?= base_url('matakuliah'); ?>">Kembali</a>
-            </td>
-        </tr>
-    </table>
-
+    <center>
+        <table>
+            <tr>
+                <th colspan="3">
+                    Tampil Data Mata Kuliah
+                </th>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <hr>
+                </td>
+            </tr>
+            <tr>
+                <th>Kode MTK</th>
+                <th>:</th>
+                <td>
+                    <?= $kode; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Nama MTK</td>
+                <td>:</td>
+                <td>
+                    <?= $nama; ?>
+                </td>
+            </tr>
+            <tr>
+                <td>SKS</td>
+                <td>:</td>
+                <td>
+                    <?= $sks; ?>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" align="center">
+                    <a href="<?= base_url('matakuliah'); ?>">Kembali</a>
+                </td>
+            </tr>
+        </table>
+    </center>
 
 </body>
 
